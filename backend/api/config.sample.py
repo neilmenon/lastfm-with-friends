48d35e790fda653f8ed2fdd5d607c37a696bd841
neilmenon <steelneil12@gmail.com>
2020-12-24 14:24:00 -0500

    backend config file w/ sample

---
+++ backend/api/config.sample.py
@@ -0,0 +1,12 @@
+# rename this to config.py and fill in the fields
+
+config = {
+    'host': '',
+    'port': 3306,
+    'user': '',
+    'db': '',
+    'api': {
+        'key': '', # Last.fm API key. See https://www.last.fm/api/account/create
+        'secret': '', # Last.fm Secret key. See https://www.last.fm/api/account/create
+    }
+}
--- backend/api/users.py
+++ backend/api/users.py
@@ -1,7 +1,11 @@
 import flask
+import json
+import mariadb
+from . import config
+cfg = config.config
 
 user_api = flask.Blueprint('users', __name__)
 
 @user_api.route('/api/users')
 def users():
-    return flask.jsonify("{'users': 0}")
+    return flask.jsonify({'users': 0})
