a762d0376c8844da0807003f0d1a023c68637b36
neilmenon <steelneil12@gmail.com>
2020-12-24 19:55:24 -0500

    routes for CREATE and READ users

--- backend/api/config.sample.py
+++ backend/api/config.sample.py
@@ -1,10 +1,11 @@
-# rename this to config.py and fill in the fields
-
 config = {
-    'host': '',
-    'port': 3306,
-    'user': '',
-    'db': '',
+    'sql': {
+        'user': '',
+        'password': '',
+        'host': '',
+        'port': 3306,
+        'database': 'lastfm-with-friends',
+    },
     'api': {
         'key': '', # Last.fm API key. See https://www.last.fm/api/account/create
         'secret': '', # Last.fm Secret key. See https://www.last.fm/api/account/create
--- backend/api/users.py
+++ backend/api/users.py
@@ -1,11 +1,45 @@
-import flask
+import werkzeug
+import sys
+from flask import *
 import json
 import mariadb
 from . import config
+from . import sql_helper
 cfg = config.config
 
-user_api = flask.Blueprint('users', __name__)
+user_api = Blueprint('users', __name__)
 
-@user_api.route('/api/users')
-def users():
-    return flask.jsonify({'users': 0})
+mdb = mariadb.connect(**(cfg['sql']))
+cursor = mdb.cursor(dictionary=True)
+
+@user_api.route('/api/users', methods=['POST'])
+def create():
+    try:
+        params = request.args
+        return "Success"
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        response = make_response(jsonify(error="Database error: " + str(e)), 500)
+        abort(response)
+    except KeyError as e:
+        response = make_response(jsonify(error="Missing required parameter '" + str(e.args[0]) + "'"), 400)
+        abort(response)
+
+@user_api.route('/api/users/<int:user_id>', methods=['GET'])
+def get(user_id):
+    try:
+        params = request.args
+        cursor.execute("SELECT * FROM users WHERE user_id = " + str(user_id) + ";")
+        result = list(cursor)
+        if result:
+            return jsonify(result[0])
+        else:
+            response = make_response(jsonify(error="User with ID " + str(user_id) + " not found."), 404)
+            abort(response)
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        response = make_response(jsonify(error="Database error: " + str(e)), 500)
+        abort(response)
+    except KeyError as e:
+        response = make_response(jsonify(error="Missing required parameter '" + str(e.args[0]) + "'."), 400)
+        abort(response)
