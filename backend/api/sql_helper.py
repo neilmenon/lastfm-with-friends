19bc890675f8aa1eb528c1fd6dee80a3f47b20ca
neilmenon <nkm5gz@virginia.edu>
2020-12-26 00:25:57 -0500

    more work on create user endpoint

--- backend/api/sql_helper.py
+++ backend/api/sql_helper.py
@@ -2,5 +2,5 @@ def insert_into_where_not_exists(table, data, unique):
     columns = ', '.join("`" + str(x).replace('/', '_') + "`" for x in data.keys())
     values = ', '.join("'" + str(x).replace('/', '_') + "'" for x in data.values())
     sql = "INSERT INTO %s ( %s ) SELECT %s" % (table, columns, values)
-    sql += " FROM DUAL WHERE NOT EXISTS (SELECT name FROM "+table+" WHERE " + unique + " = \""+data[unique]+"\");"
+    sql += " FROM DUAL WHERE NOT EXISTS (SELECT * FROM "+table+" WHERE " + unique + " = \""+data[unique]+"\");"
     return sql
--- backend/api/users.py
+++ backend/api/users.py
@@ -3,34 +3,53 @@ import sys
 from flask import *
 import json
 import mariadb
+import datetime
 from . import config
 from . import sql_helper
 cfg = config.config
 
 user_api = Blueprint('users', __name__)
 
-mdb = mariadb.connect(**(cfg['sql']))
-cursor = mdb.cursor(dictionary=True)
 
 @user_api.route('/api/users', methods=['POST'])
 def create():
     try:
-        params = request.args
-        return "Success"
+        mdb = mariadb.connect(**(cfg['sql']))
+        cursor = mdb.cursor(dictionary=True)
+        params = request.get_json()
+        data = {
+            "username": params['username'],
+            "display_name": params['display_name'],
+            "registered": datetime.datetime.utcfromtimestamp(int(params['registered'])),
+            "profile_image": params['profile_image'],
+        }
+        sql = sql_helper.insert_into_where_not_exists("users", data, "username")
+        # return sql
+        result = cursor.execute(sql)
+        mdb.commit()
+        mdb.close()
+        if cursor.rowcount > 0:
+            return jsonify({"success": "Successfuly created user " + params['username'] +"."})
+        else:
+            response = make_response(jsonify(error="User already exists."), 400)
+            abort(response)
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
         response = make_response(jsonify(error="Database error: " + str(e)), 500)
         abort(response)
     except KeyError as e:
-        response = make_response(jsonify(error="Missing required parameter '" + str(e.args[0]) + "'"), 400)
+        response = make_response(jsonify(error="Missing required parameter: " + str(e.args[0]) + "."), 400)
         abort(response)
 
 @user_api.route('/api/users/<int:user_id>', methods=['GET'])
 def get(user_id):
     try:
+        mdb = mariadb.connect(**(cfg['sql']))
+        cursor = mdb.cursor(dictionary=True)
         params = request.args
         cursor.execute("SELECT * FROM users WHERE user_id = " + str(user_id) + ";")
         result = list(cursor)
+        mdb.close()
         if result:
             return jsonify(result[0])
         else:
