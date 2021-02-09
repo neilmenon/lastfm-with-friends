3a25945fdfea80d5e650601f25fd1793ef085b57
neilmenon <steelneil12@gmail.com>
2021-02-09 00:39:00 -0500

    bugfix wk: use '=' then LIKE as fallback (thx Kal)

--- backend/api/command_helper.py
+++ backend/api/command_helper.py
@@ -8,28 +8,45 @@ from . import api_logger as logger
 
 cfg = config.config
 
-def wk_artist(query, users):
+def find_artist(query):
     mdb = mariadb.connect(**(cfg['sql']))
     cursor = mdb.cursor(dictionary=True)
 
     # find artist in the database
     sanitized_query = sql_helper.sanitize_query(query)
-    sql = "SELECT * from artists WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("name"), sanitized_query)
+    sql = "SELECT * from artists WHERE UPPER({}) = UPPER('%{}%')".format(sql_helper.sanitize_db_field("name"), sanitized_query)
     cursor.execute(sql)
     result = list(cursor)
     if not result:
-        sql = "SELECT * from artist_redirects WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("artist_name"), sql_helper.esc_db(query))
+        # fallback to LIKE
+        sql = "SELECT * from artists WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("name"), sanitized_query)
         cursor.execute(sql)
         result = list(cursor)
         if not result:
-            mdb.close()
-            return None
-        redirected_name = result[0]['redirected_name']
-        sql = "SELECT * from artists WHERE name = '{}'".format(sql_helper.esc_db(redirected_name))
-        cursor.execute(sql)
-        result = list(cursor)
+            # check artist redirects
+            sql = "SELECT * from artist_redirects WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("artist_name"), sql_helper.esc_db(query))
+            cursor.execute(sql)
+            result = list(cursor)
+            if not result:
+                mdb.close()
+                return None
+            redirected_name = result[0]['redirected_name']
+            sql = "SELECT * from artists WHERE name = '{}'".format(sql_helper.esc_db(redirected_name))
+            cursor.execute(sql)
+            result = list(cursor)
+            artist = result[0]
         artist = result[0]
     artist = result[0]
+    mdb.close()
+    return artist
+
+def wk_artist(query, users):
+    artist = find_artist(query)
+    if not artist:
+        return None
+
+    mdb = mariadb.connect(**(cfg['sql']))
+    cursor = mdb.cursor(dictionary=True)
 
     # find users who have scrobbled this artist
     users_list = ", ".join(str(u) for u in users)
@@ -41,42 +58,31 @@ def wk_artist(query, users):
     return {'artist': artist, 'users': result, 'total_scrobbles': total_scrobbles, 'total_users': len(users)}
 
 def wk_album(query, users):
-    mdb = mariadb.connect(**(cfg['sql']))
-    cursor = mdb.cursor(dictionary=True)
-
     query = sql_helper.sanitize_query(query)
     try:
         artist_query = query.strip().split(" - ", 1)[0].strip()
         album_query = query.strip().split(" - ", 1)[1].strip()
     except IndexError:
-        mdb.close()
         return False
 
-    # find artist in the database
-    sql = "SELECT * from artists WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("name"), sql_helper.esc_db(artist_query))
+    artist = find_artist(artist_query)
+    if not artist:
+        return None
+
+    mdb = mariadb.connect(**(cfg['sql']))
+    cursor = mdb.cursor(dictionary=True)
+
+    # find album in the database
+    sql = "SELECT * from albums WHERE artist_name = '{}' AND UPPER({}) = UPPER('{}')".format(sql_helper.esc_db(artist['name']), sql_helper.sanitize_db_field("name"), sql_helper.esc_db(album_query))
     cursor.execute(sql)
     result = list(cursor)
     if not result:
-        sql = "SELECT * from artist_redirects WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("artist_name"), sql_helper.esc_db(artist_query))
+        sql = "SELECT * from albums WHERE artist_name = '{}' AND {} LIKE '%{}%'".format(sql_helper.esc_db(artist['name']), sql_helper.sanitize_db_field("name"), sql_helper.esc_db(album_query))
         cursor.execute(sql)
         result = list(cursor)
         if not result:
             mdb.close()
             return None
-        redirected_name = result[0]['redirected_name']
-        sql = "SELECT * from artists WHERE name = '{}'".format(sql_helper.esc_db(redirected_name))
-        cursor.execute(sql)
-        result = list(cursor)
-        artist = result[0]
-    artist = result[0]
-
-    # find album in the database
-    sql = "SELECT * from albums WHERE artist_name = '{}' AND {} LIKE '%{}%'".format(sql_helper.esc_db(artist['name']), sql_helper.sanitize_db_field("name"), sql_helper.esc_db(album_query))
-    cursor.execute(sql)
-    result = list(cursor)
-    if not result:
-        mdb.close()
-        return None
     album = result[0]
 
     # find users who have scrobbled this album
@@ -89,42 +95,33 @@ def wk_album(query, users):
     return {'artist': artist, 'album': album, 'users': result, 'total_scrobbles': total_scrobbles, 'total_users': len(users)}
 
 def wk_track(query, users):
-    mdb = mariadb.connect(**(cfg['sql']))
-    cursor = mdb.cursor(dictionary=True)
-
     query = sql_helper.sanitize_query(query)
     try:
         artist_query = query.strip().split(" - ", 1)[0].strip()
         track_query = query.strip().split(" - " , 1)[1].strip()
     except IndexError:
-        mdb.close()
         return False
 
-    # find artist in the database
-    sql = "SELECT * from artists WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("name"), sql_helper.esc_db(artist_query))
+    artist = find_artist(artist_query)
+    if not artist:
+        return None
+
+    mdb = mariadb.connect(**(cfg['sql']))
+    cursor = mdb.cursor(dictionary=True)
+
+    # find track in the database
+    sql = "SELECT DISTINCT track as name, albums.image_url, albums.name as album_name from track_scrobbles LEFT JOIN albums ON track_scrobbles.album_id = albums.id WHERE artist_id = {} AND UPPER({}) = UPPER('{}')".format(artist['id'], sql_helper.sanitize_db_field("track"), sql_helper.esc_db(track_query))
+    logger.log(sql)
     cursor.execute(sql)
     result = list(cursor)
     if not result:
-        sql = "SELECT * from artist_redirects WHERE {} LIKE '%{}%'".format(sql_helper.sanitize_db_field("artist_name"), sql_helper.esc_db(artist_query))
+        sql = "SELECT DISTINCT track as name, albums.image_url, albums.name as album_name from track_scrobbles LEFT JOIN albums ON track_scrobbles.album_id = albums.id WHERE artist_id = {} AND {} LIKE '%{}%'".format(artist['id'], sql_helper.sanitize_db_field("track"), sql_helper.esc_db(track_query))
         cursor.execute(sql)
         result = list(cursor)
         if not result:
             mdb.close()
             return None
-        redirected_name = result[0]['redirected_name']
-        sql = "SELECT * from artists WHERE name = '{}'".format(sql_helper.esc_db(redirected_name))
-        cursor.execute(sql)
-        result = list(cursor)
-        artist = result[0]
-    artist = result[0]
-
-    # find track in the database
-    sql = "SELECT DISTINCT track as name, albums.image_url, albums.name as album_name from track_scrobbles LEFT JOIN albums ON track_scrobbles.album_id = albums.id WHERE artist_id = {} AND {} LIKE '%{}%'".format(artist['id'], sql_helper.sanitize_db_field("track"), sql_helper.esc_db(track_query))
-    cursor.execute(sql)
-    result = list(cursor)
-    if not result:
-        mdb.close()
-        return None
+        track = result[0]
     track = result[0]
     track['url'] = artist['url'] + "/" + track['album_name'].replace(" ", "+") + "/" + track['name'].replace(" ", "+")
 
